4aaec214faccd114ac49f294a1c36880fb9aca91
Rickard Jonsson <rickaard@gmail.com>
2023-11-13 21:06:56 +0100

    Display problems scoreboard

--- lib/climbcomp_web/controllers/page_html.ex
+++ lib/climbcomp_web/controllers/page_html.ex
@@ -1,5 +1,16 @@
 defmodule ClimbcompWeb.PageHTML do
   use ClimbcompWeb, :html
 
+  alias Climbcomp.Result
+  alias Climbcomp.Problem
+
   embed_templates "page_html/*"
+
+  defp results_for_problem(results, problem_id) do
+    Enum.filter(results, &(&1.problem_id == problem_id))
+  end
+
+  defp topped_problem?(%Result{points: points}, %Problem{toppoints: toppoints}) do
+    points == toppoints
+  end
 end
